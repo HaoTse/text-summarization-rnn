e7f8d0d89cd84d4e8626e7a6fa61e716f6fdc47d
top30339 <top30339@gmail.com>
2018-01-20 14:38:14 +0800

    Add data-preprocess/count.py
    - Use to count the segmented word length of broken file which used to
    define bucket.
    - Update broken.py.

--- data-preprocess/broken.py
+++ data-preprocess/broken.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import jieba
 import re
 import logging
@@ -36,74 +38,92 @@ def process(s):
     return s
 
 # sqgment the input
-def SeqSentence(s, mode = ''):
-    s = FullToHalf(s)
-    s = process(s)
-    s = jieba.cut(s.strip(), cut_all=False)
-    outstr = ''
-    cnt = 0
-    for word in s:
-        if (mode == 'title' and cnt > 30) or (mode == 'content' and cnt > 120):
-            break
+def SeqSentence(title, content):
+    title = FullToHalf(title)
+    title = process(title)
+    title = jieba.cut(title.strip(), cut_all=False)
+
+    content = FullToHalf(content)
+    content = process(content)
+    content = jieba.cut(content.strip(), cut_all=False)
+
+    title_list, content_list = [], []
+
+    for word in content:
         if word not in stopwords and word != '\t' and word != ' ' and word != '\ue40c':
-            outstr += (word + ' ')
-            if word not in voca:
-                voca.append(word)
-            cnt += 1
-    return outstr + '\n'
+            content_list.append(word)
+
+    if len(content_list) > 250 or len(content_list) < 30:
+        return None, None
+
+    for word in title:
+        if word not in stopwords and word != '\t' and word != ' ' and word != '\ue40c':
+            title_list.append(word)
+
+    if len(title_list) < 5:
+        return None, None
+
+    title_str = ' '.join(title_list) + '\n'
+    content_str = ' '.join(content_list) + '\n'
+    return title_str, content_str
 
 def main():
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+    title_file = 'title_broken'
+    content_file = 'content_broken'
+
     # empty file
-    open('title_broken', 'w').close()
-    open('content_broken', 'w').close()
+    open(title_file, 'w').close()
+    open(content_file, 'w').close()
     
     # deal with title
     with open('title', 'r', encoding="utf-8") as f:
-        content = f.readlines()
-
-    output = ''
-    cnt = 0
-    for line in content:
-        words = SeqSentence(line, 'title')
-        output += (words)
-        cnt += 1
-        if not (cnt % 10000):
-            logging.info("已處理 %d 篇title" % cnt)
-            with open('title_broken', 'a', encoding="utf-8") as f:
-                f.write(output)
-            output = ''
-    title_num = cnt
-    with open('title_broken', 'a', encoding="utf-8") as f:
-        f.write(output)
-
-    # deal with content
+        title_lines = f.readlines()
     with open('content', 'r', encoding="utf-8") as f:
-        content = f.readlines()
+        content_lines = f.readlines()
 
-    output = ''
+    if len(title_lines) != len(content_lines):
+        logging.error('長度不相同')
+
+    title_output, content_output = '', ''
     cnt = 0
-    for line in content:
-        words = SeqSentence(line, 'content')
-        output += (words)
+    for i in range(len(title_lines)):
+        title_line = title_lines[i]
+        content_line = content_lines[i]
+
+        if '產品名稱' in content_line or '常見問題' in content_line or '綜合評價' in content_line:
+            continue
+
+        title_str, content_str = SeqSentence(title_line, content_line)
+        
+        if not title_str or not content_str:
+            continue
+
+        title_output += (title_str)
+        content_output += (content_str)
         cnt += 1
         if not (cnt % 10000):
-            logging.info("已處理 %d 篇content" % cnt)
-            with open('content_broken', 'a', encoding="utf-8") as f:
-                f.write(output)
-            output = ''
-    content_num = cnt
-    with open('content_broken', 'a', encoding="utf-8") as f:
-        f.write(output)
+            logging.info("已處理 %d 篇" % cnt)
+            with open(title_file, 'a', encoding="utf-8") as f:
+                f.write(title_output)
+            title_output = ''
+            with open(content_file, 'a', encoding="utf-8") as f:
+                f.write(content_output)
+            content_output = ''
+
+    with open(title_file, 'a', encoding="utf-8") as f:
+        f.write(title_output)
+    with open(content_file, 'a', encoding="utf-8") as f:
+        f.write(content_output)
     
     # output vocabulary
-    with open('voca', 'w', encoding="utf-8") as f:
-        for word in voca:
-            f.write("%s\n" % word)
+    # with open('voca', 'w', encoding="utf-8") as f:
+    #     for word in voca:
+    #         f.write("%s\n" % word)
     
-    logging.info("共處理 (%d, %d) 篇", title_num, content_num)
-    logging.info("vocabulary 大小為 %d", len(voca))
+    logging.info("共處理 %d 篇", cnt)
+    # logging.info("vocabulary 大小為 %d", len(voca))
 
 if __name__ == '__main__':
     main()
---
+++ data-preprocess/count.py
@@ -0,0 +1,42 @@
+import logging
+
+def main():
+    logging.basicConfig(format = '%(asctime)s : %(levelname)s : %(message)s', level = logging.INFO)
+
+    content_file = 'content_broken'
+    title_file = 'title_broken'
+    total = 554668.
+
+    with open(content_file, 'r', encoding='utf-8') as f:
+        content_lines = f.readlines()
+    with open(title_file, 'r', encoding='utf-8') as f:
+        title_lines = f.readlines()
+
+    content_len, title_len = 0.0, 0.0
+    content_max, title_max = 0, 0
+    content_min, title_min = 1000, 1000
+    for i in range(round(total)):
+        content_line = content_lines[i]
+        title_line = title_lines[i]
+        
+        content_word = content_line.strip().split(" ")
+        title_word = title_line.strip().split(" ")
+
+        content_len += len(content_word) / total
+        content_max = len(content_word) if len(content_word) > content_max else content_max
+        content_min = len(content_word) if len(content_word) < content_min else content_min
+        if len(content_word) == 1:
+            print (i)
+
+        title_len += len(title_word) / total
+        title_max = len(title_word) if len(title_word) > title_max else title_max
+        title_min = len(title_word) if len(title_word) < title_min else title_min
+
+        if not i % 10000:
+            logging.info("Process %d lines" % i)
+
+    logging.info("(average length, max length, min length) of content: (%.2f, %d, %d)" % (content_len, content_max, content_min))
+    logging.info("(average length, max length, min length) of title: (%.2f, %d, %d)" % (title_len, title_max, title_min))
+
+if __name__ == '__main__':
+    main()
